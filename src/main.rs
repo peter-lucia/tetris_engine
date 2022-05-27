2a57ddf3ed772fa508532bcaa0812e992efb7f42
Peter Lucia <plucia19@gmail.com>
2022-05-26 22:44:42 -0700

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use crate::board::{Board, BoardCommandLine};
-
 mod board;
 mod tetromino;
 
 fn main() {
-    println!("Hello, world!");
     let board: Board = BoardCommandLine::new();
     board.run()
 }
