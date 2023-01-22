af4c7ddb407de42457b8127e0a009830a87648aa
Peter Lucia <peter-lucia@users.noreply.github.com>
2023-01-21 23:48:06 -0800

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
 use std::time::Duration;
 use crate::well::{Tetris, Well, WELL_HEIGHT, WELL_WIDTH};
-use serde_json::{json};
+use serde_json::{json, to_string};
+use serde_json;
 use uuid::{Uuid, uuid};
 use std::collections::HashMap;
 use rocket::http::RawStr;
@@ -16,7 +17,7 @@ mod tetromino;
 mod well;
 
 lazy_static! {
-    static ref ACTIVE_GAMES: Mutex<HashMap<Uuid, Well>> = {
+    static ref ACTIVE_GAMES: Mutex<HashMap<String, Well>> = {
         let mut m = HashMap::new();
         Mutex::new(m)
     };
@@ -34,7 +35,7 @@ fn setup_game() -> String {
     let mut t: Well = Tetris::new();
     let id = Uuid::new_v4();
     let serialized = serde_json::to_string(&t).unwrap();
-    map.insert(id, t);
+    map.insert(id.to_string(), t);
     return serialized;
 }
 
@@ -43,17 +44,28 @@ fn move_left() -> String {
     "OK".to_string()  // placeholder
 }
 
-#[post("/right", data = "<id>")]
-fn move_right(id: &RawStr) -> String {
-    let unique_id: Uuid = Uuid::parse_str(id.as_str()).unwrap();
-    let mut guard: MutexGuard<HashMap<Uuid, Well>> = ACTIVE_GAMES.lock().unwrap();
+#[post("/right", data = "<req>")]
+fn move_right(req: &str) -> String {
+
+    let binding: serde_json::Value = serde_json::from_str(req).unwrap();
+    let id = binding.get("id").unwrap();
+    let mut hashmap_guard: MutexGuard<HashMap<String, Well>> = ACTIVE_GAMES.lock().unwrap();
+    if hashmap_guard.get(id.to_string().as_str()).is_none() {
+        log::info!("Missing id {id}", id=id);
+        return json!({
+            "status": format!("Missing id: {id}", id = id),
+            "data": {
+                "id": id,
+            }
+        }).to_string();
+    }
     // must clone the original reference
-    let mut well: Well = guard.get(&unique_id).cloned().unwrap();
+    let mut well: Well = hashmap_guard.get(&id.to_string()).cloned().unwrap();
     well.move_right();
     // create the json response
     let result = serde_json::to_string(&well).unwrap();
     // update the active games
-    guard.insert(unique_id, well);
+    hashmap_guard.insert(id.to_string(), well);
     return result;
 }
 
