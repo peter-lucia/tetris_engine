de50765d0c3b8c9a93ee1fb10a30c4c2ed4a9b1f
Peter Lucia <peter-lucia@users.noreply.github.com>
2023-03-09 22:09:24 -0800

    Working on package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="tetris-engine",
+    version="0.1.0",
+    rust_extensions=[RustExtension("hello_rust.hello_rust", binding=Binding.PyO3)],
+    packages=["tetris_engine"],
+    zip_safe=False,  # rust extensions are not zip safe, just like C-extensions.
+)
