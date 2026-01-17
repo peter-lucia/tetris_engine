7bcff117aab09d898c762a7dc4125910a27efdd9
Peter Lucia <peter-lucia@users.noreply.github.com>
2026-01-17 10:21:11 -0800

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ mod well;
 
 
 fn main() {
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+    log::info!("Starting Tetris...");
     let mut t: Well = Tetris::new();
     t.setup();
     t.run_game();
